080ee393d3f890d513cb2777d1ea0f0cbcdfdaf9
Alexander Kremenets <kremenets0207@gmail.com>
2017-01-27 21:06:32 +0300

    Update i18nify and fix built files

--- build/actions.js
+++ build/actions.js
@@ -10,21 +10,21 @@ var _index = require('./index');
 var LOAD_TRANSLATIONS = exports.LOAD_TRANSLATIONS = 'loadTranslation';
 var loadTranslations = exports.loadTranslations = function loadTranslations(translations) {
   return function (dispatch) {
-    _index.I18n.forceComponentsUpdate();
     dispatch({
       type: LOAD_TRANSLATIONS,
       translations: translations
     });
+    _index.I18n.forceComponentsUpdate();
   };
 };
 
 var SET_LOCALE = exports.SET_LOCALE = 'setLocal';
 var setLocale = exports.setLocale = function setLocale(locale) {
   return function (dispatch) {
-    _index.I18n.forceComponentsUpdate();
     dispatch({
       type: SET_LOCALE,
       locale: locale
     });
+    _index.I18n.forceComponentsUpdate();
   };
 };
